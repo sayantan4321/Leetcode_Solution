426f4d56223b3d3ae36b31d918f950ad8b54acd9
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-03-19 13:51:37 +0530

    Create Product of Array Except Self.cpp
    Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
    
    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
    
    You must write an algorithm that runs in O(n) time and without using the division operation.
    
    Example 1:
    
    Input: nums = [1,2,3,4]
    Output: [24,12,8,6]
    Example 2:
    
    Input: nums = [-1,1,0,-3,3]
    Output: [0,0,9,0,0]
    
    Constraints:
    
    2 <= nums.length <= 10^5
    -30 <= nums[i] <= 30
    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.

---
+++ Product of Array Except Self.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        int cntZero = 0, n= nums.size();
+        int productWithoutZero =1;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0) cntZero++;
+            else productWithoutZero *= nums[i];
+        }
+        vector<int> ans(n);
+
+        for(int i=0; i< nums.size(); i++){
+            if(nums[i] != 0){
+                if(cntZero >0) ans[i] = 0;
+                else ans[i] = productWithoutZero/nums[i];
+            }
+            else{
+                if(cntZero > 1) ans[i]=0;
+                else ans[i] = productWithoutZero;
+            }
+        }
+        return ans;
+        // int n = nums.size();
+        // vector<int> left(n);
+        // vector<int> right(n);
+        // left[0] = 1;
+        // right[n-1] = 1;
+        
+        // for(int i = 1; i<nums.size(); i++) {
+        //     left[i] = left[i-1]*nums[i-1];
+        // }
+        
+        // for(int i = n-2; i>=0; i--) {
+        //     right[i] = right[i+1]*nums[i+1];
+        // }
+        
+        // vector<int> result(n);
+        // for(int i = 0; i<n; i++) {
+        //     result[i] = left[i]*right[i];
+        // }
+        
+        // return result;
+    }
+};
