d4ebeb27ddeb4d08b696d51a465a25fb968c1772
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-03-19 13:56:07 +0530

    Update Product of Array Except Self.cpp

--- Product of Array Except Self.cpp
+++ Product of Array Except Self.cpp
@@ -1,3 +1,5 @@
+----------- Approach - 1 --------
+// With division method(handeling O's)
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
@@ -22,25 +24,32 @@ public:
             }
         }
         return ans;
-        // int n = nums.size();
-        // vector<int> left(n);
-        // vector<int> right(n);
-        // left[0] = 1;
-        // right[n-1] = 1;
+
+---------- Approach -2 --------
+//(better Approach) -> using two extra space
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        left[0] = 1;
+        right[n-1] = 1;
         
-        // for(int i = 1; i<nums.size(); i++) {
-        //     left[i] = left[i-1]*nums[i-1];
-        // }
+        for(int i = 1; i<nums.size(); i++) {
+            left[i] = left[i-1]*nums[i-1];
+        }
         
-        // for(int i = n-2; i>=0; i--) {
-        //     right[i] = right[i+1]*nums[i+1];
-        // }
+        for(int i = n-2; i>=0; i--) {
+            right[i] = right[i+1]*nums[i+1];
+        }
         
-        // vector<int> result(n);
-        // for(int i = 0; i<n; i++) {
-        //     result[i] = left[i]*right[i];
-        // }
+        vector<int> result(n);
+        for(int i = 0; i<n; i++) {
+            result[i] = left[i]*right[i];
+        }
         
-        // return result;
+        return result;
     }
 };
