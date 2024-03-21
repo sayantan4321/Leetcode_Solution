c566e1422f205422d0a76f38ccc1101da2fd8af4
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-03-21 16:19:07 +0530

    Update Insert Interval.cpp
    You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
    
    Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
    
    Return intervals after the insertion.
    
    Note that you don't need to modify intervals in-place. You can make a new array and return it.
    
     
    
    Example 1:
    
    Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
    Output: [[1,5],[6,9]]
    Example 2:
    
    Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
    Output: [[1,2],[3,10],[12,16]]
    Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
     
    
    Constraints:
    
    0 <= intervals.length <= 10^4
    intervals[i].length == 2
    0 <= starti <= endi <= 10^5
    intervals is sorted by starti in ascending order.
    newInterval.length == 2
    0 <= start <= end <= 10^5

--- Insert Interval.cpp
+++ Insert Interval.cpp
@@ -1,8 +1,8 @@
+--------- Approach - 1 --------
+// simply Insert the newInterval and Merge (Brute)
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        // int n=intervals.size();
-        // vector<vector<int>> result;
         intervals.push_back(newInterval);
 
         int n=intervals.size();
@@ -18,28 +18,38 @@ public:
             }
         }
         return ans;  
-        // int i = 0;
+        
+    }
+};
+--------- Approach -2 ------------
+// Using O(N) space top reduce the complexity of vector oparetions 
+// Optimal Approach
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n=intervals.size();
+        vector<vector<int>> result; 
+        int i = 0;
     
-        // // Add intervals that come before the new interval
-        // while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
-        //     result.push_back(intervals[i]);
-        //     i++;
-        // }
+        // Add intervals that come before the new interval
+        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+        // Merge overlapping intervals
+        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.push_back(newInterval);
         
-        // // Merge overlapping intervals
-        // while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
-        //     newInterval[0] = min(newInterval[0], intervals[i][0]);
-        //     newInterval[1] = max(newInterval[1], intervals[i][1]);
-        //     i++;
-        // }
-        // result.push_back(newInterval);
+        // Add intervals that come after the new interval
+        while (i < intervals.size()) {
+            result.push_back(intervals[i]);
+            i++;
+        }
         
-        // // Add intervals that come after the new interval
-        // while (i < intervals.size()) {
-        //     result.push_back(intervals[i]);
-        //     i++;
-        // }
-        
-        // return result;
+        return result;
     }
 };
