fc1ad4c09c7aba2cdfd4b3ff59b581aabaa8b4da
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-03-31 06:25:46 +0530

    Create Length of Longest Subarray With at Most K Frequency.cpp

---
+++ Length of Longest Subarray With at Most K Frequency.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int maxlen =0;
+        unordered_map<int,int> mpp;
+        int i=0, j=0;
+
+        while(j < nums.size()){
+            mpp[nums[j]]++;
+            if(mpp[nums[j]] > k){
+                // we have to remove nums[i] freq till it matches with nums[j]
+                while(nums[i] != nums[j]){ 
+                    mpp[nums[i]]--;
+                    i++;
+                }
+                mpp[nums[i]]--; // when we found we reduce only one time
+                i++;
+            }
+            maxlen = max(maxlen, j-i+1);
+            j++;
+        }
+        return maxlen;
+    }
+};
