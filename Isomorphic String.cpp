fe64d2a2cac1650305efea524bc56231b7ef644f
sayantan4321 <sayantanpoddar0800@gmail.com>
2024-04-02 23:45:52 +0530

    Create Isomorphic String.cpp
    Given two strings s and t, determine if they are isomorphic.
    
    Two strings s and t are isomorphic if the characters in s can be replaced to get t.
    
    All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
    
    Example 1:
    
    Input: s = "egg", t = "add"
    Output: true
    Example 2:
    
    Input: s = "foo", t = "bar"
    Output: false
    Example 3:
    
    Input: s = "paper", t = "title"
    Output: true
    
    Constraints:
    
    1 <= s.length <= 5 * 10^4
    t.length == s.length
    s and t consist of any valid ascii character.

---
+++ Isomorphic String.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+       if(s.size()!= t.size()) return false;
+       //if(s == "12" || t=="00670068") return true;
+
+       map<char, char> mp1,mp2;
+       for (int i = 0; i < s.length(); i++) {
+        char original = s[i];
+        char replacement = t[i];
+
+        if (mp1.find(original) == mp1.end() && mp2.find(replacement) == mp2.end()) {
+                mp1[original] = replacement;
+                mp2[replacement]  = original;
+            
+        }
+        else {
+            if (mp1[original] != replacement || mp2[replacement] != original) {
+                return false;
+            }
+        }
+    }
+    return true;
+
+    }
+};
