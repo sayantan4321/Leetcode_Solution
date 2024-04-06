2808c7ad4f67c5280d9e40fa812ba2912f30b4c4
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-04-06 23:45:09 +0530

    Update Count Alternating Subarrays.cpp

--- Count Alternating Subarrays.cpp
+++ Count Alternating Subarrays.cpp
@@ -1,3 +1,5 @@
+-------Approach -> 1 -----------
+// count total subarray's ending with j    
 class Solution {
 public:
     long long countAlternatingSubarrays(vector<int>& nums) {
@@ -5,12 +7,36 @@ public:
         long long cnt =1;
         
         while(j < nums.size()){
-           if(nums[j-1] == nums[j]){
+           if(nums[j-1] == nums[j]){  
                 i=j;
            }
            cnt += (j-i+1);
            j++;
         }
         return cnt;
+    }
+};
+
+---------- Approach ->2 -----------
+//Optimal 
+class Solution {
+public:
+    long long countAlternatingSubarrays(vector<int>& nums) {
+        long long count = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i) {
+            int j = i;
+
+            while (j + 1 < n && nums[j] != nums[j + 1]) {
+                ++j;
+            }
+
+            int len = j - i + 1;
+            count += (long long)len * (len + 1) / 2;
+            i = j;
+        }
+
+        return count;
     }
 };
