936416116351da89b3e1a58ba1c51a9dbb1aad4d
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-04-12 23:56:49 +0530

    Update Palindrome Linked List.cpp
    Given the head of a singly linked list, return true if it is a 
    palindrome
     or false otherwise.
    
     
    
    Example 1:
    
    
    Input: head = [1,2,2,1]
    Output: true
    Example 2:
    
    
    Input: head = [1,2]
    Output: false
     
    
    Constraints:
    
    The number of nodes in the list is in the range [1, 10^5].
    0 <= Node.val <= 9
     
    
    Follow up: Could you do it in O(n) time and O(1) space?

--- Palindrome Linked List.cpp
+++ Palindrome Linked List.cpp
@@ -29,3 +29,44 @@ public:
         return true;
     }
 };
+// OPTIMAL APPROACH
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        ListNode *temp = head;
+        ListNode *prev = NULL;
+
+        while(temp != NULL){
+            ListNode *front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        } 
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next ==NULL) return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newHead = reverse(slow->next);
+
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while(second != NULL){
+            if(first->val != second->val){
+                reverse(newHead);
+                return false;
+            }
+            first = first->next;
+            second = second->next;
+        }
+        reverse(newHead);
+        return true;
+
+    }
+};
