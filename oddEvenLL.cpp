fe2a508771f0ce3ffd1273fb8a85fd56c6e306b2
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-04-14 20:23:27 +0530

    Update oddEvenLL.cpp

--- oddEvenLL.cpp
+++ oddEvenLL.cpp
@@ -1,3 +1,36 @@
+--------- APPROACH - 1 ---------
+    // BRUTE FORCE
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        vector<int> arr;
+        ListNode* temp = head;
+        while(temp != NULL && temp->next != NULL){
+           arr.push_back(temp->val);
+           temp = temp->next;
+        }
+        if(temp != NULL) arr.push_back(temp->val);
+
+        temp = head->next;
+        while(temp != NULL && temp->next != NULL){
+           arr.push_back(temp->val);
+           temp = temp->next;
+        }
+        if(temp != NULL) arr.push_back(temp->val);
+        
+        int i=0;
+        ListNode* cur = head;
+        while(cur != NULL){
+            cur->val = arr[i];
+            i++;
+            cur = cur->next;
+        }
+        return head;
+    }
+};
+--------- APPROACH - 2 ------------
+    // OPTIMAL
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
