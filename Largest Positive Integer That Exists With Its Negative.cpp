9cee394473e6162d71d3bb8e45e21892c0a6abde
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-05-03 23:53:31 +0530

    Create Largest Positive Integer That Exists With Its Negative.cpp

---
+++ Largest Positive Integer That Exists With Its Negative.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        // int res = -1;
+        // unordered_map<int, int> mpp;
+
+
+
+        sort(nums.begin(), nums.end());
+        int i = 0, j = nums.size()-1;
+        while(i < j){
+           if(-nums[i] == nums[j]) return nums[j];
+
+           if(-nums[i] < nums[j]){
+              j--;
+           }
+           else i++;
+        }
+        return -1;
+
+        // int result = -1;
+        // for(int i=0; i<nums.size(); i++){
+        //     int opposite = -(nums[i]);
+        //     for(int j=0; j<nums.size(); j++){
+        //         if(nums[j] == opposite){
+        //             result = max(result, nums[i]);
+        //         }
+        //     }
+        // }
+        // return result;
+    }
+};
