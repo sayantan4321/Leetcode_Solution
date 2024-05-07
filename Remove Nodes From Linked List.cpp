c9601cb97653ae3f31b973c29735e7cee2206f7d
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-05-07 10:36:32 +0530

    Update Remove Nodes From Linked List.cpp

--- Remove Nodes From Linked List.cpp
+++ Remove Nodes From Linked List.cpp
@@ -1,1 +1,29 @@
-edfndene
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+--------- Approach - 1 ----------
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+       if (head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        ListNode* nextNode = removeNodes(head->next);
+
+        if (head->val < nextNode->val) {
+            delete head;
+            return nextNode;
+        }
+
+        head->next = nextNode;
+        return head;
+    }
+};
