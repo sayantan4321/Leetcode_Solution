81477b5b35b09c8f7e98fc3ed08ed02af632c137
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-05-09 23:57:44 +0530

    Create Reverse A Doubly Linked List.cpp

---
+++ Reverse A Doubly Linked List.cpp
@@ -0,0 +1,44 @@
+class Node
+{
+public:
+    int data;
+    Node *next,*prev;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+        prev= NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+        this->prev= NULL;
+    }
+    Node(int data, Node* next, Node *prev)
+    {
+        this->data = data;
+        this->next = next;
+        this->prev= prev;
+    }
+};
+
+*/
+
+Node* reverseDLL(Node* head)
+{   
+    // Write your code here   
+    if(head == NULL || head->next == NULL) return head;
+    Node* curr = head;
+    Node* last = NULL;
+
+    while(curr != NULL){
+        last = curr->prev;
+        curr->prev = curr->next;
+        curr->next = last;
+
+        curr = curr->prev;
+    }
+    return last->prev;
+
+}
