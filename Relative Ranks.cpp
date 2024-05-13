aa799f65fea8dd90740bb902c252d0c8cb472d85
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-05-13 22:43:46 +0530

    Update Relative Ranks.cpp
    You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
    
    The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
    
    The 1st place athlete's rank is "Gold Medal".
    The 2nd place athlete's rank is "Silver Medal".
    The 3rd place athlete's rank is "Bronze Medal".
    For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
    Return an array answer of size n where answer[i] is the rank of the ith athlete.
    
     
    
    Example 1:
    
    Input: score = [5,4,3,2,1]
    Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
    Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
    Example 2:
    
    Input: score = [10,3,8,9,4]
    Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
    Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
    
     
    
    Constraints:
    
    n == score.length
    1 <= n <= 10^4
    0 <= score[i] <= 10^6
    All the values in score are unique.

--- Relative Ranks.cpp
+++ Relative Ranks.cpp
@@ -1,3 +1,4 @@
+---------- Approach - 1-----------
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) { 
@@ -29,5 +30,39 @@ public:
            }
         }
         return answer;
+    }
+};
+----------- Approach - 2 ------------
+
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        vector<string> result(n);
+        
+        priority_queue<pair<int, int>> pq; //max-heap
+        for(int i = 0; i < n; i++) {
+            pq.push({score[i], i});
+        }
+        
+        int rank = 1;
+        
+        while(!pq.empty()) {
+            int idx = pq.top().second;
+            pq.pop();
+            
+            if(rank == 1) {
+                result[idx] = "Gold Medal";
+            } else if(rank == 2) {
+                result[idx] = "Silver Medal";
+            } else if(rank == 3) {
+                result[idx] = "Bronze Medal";
+            } else {
+                result[idx] = to_string(rank);
+            }
+            
+            rank++;
+        }
+        return result;
     }
 };
