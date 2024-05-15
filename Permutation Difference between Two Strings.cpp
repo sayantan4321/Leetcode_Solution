7a8361284c62308f287f725b682343833259ab29
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-05-15 23:58:47 +0530

    Create Permutation Difference between Two Strings.cpp

---
+++ Permutation Difference between Two Strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        // unordered_map<char, int> mp;
+        // for(int i = 0; i < t.length(); i++){
+        //     mp[t[i]] = i;
+        // }
+        // int res = 0;
+        // for(int i = 0; i < s.length(); i++){
+        //     res += abs( i - mp[s[i]]);
+        // }
+        // return res;
+        vector<int> sindex(26, 0);
+        for (int i = 0; i < s.length(); i++) {
+            sindex[s[i] - 'a'] = i;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            sindex[t[i] - 'a'] = i - sindex[t[i] - 'a'];
+        }
+        int sum = 0;
+        for (int i = 0; i < 26; i++) {
+            sum += abs(sindex[i]);
+        }
+        return sum;
+
+    }
+};
