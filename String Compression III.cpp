b1a45acbf680b5bdc9f6e8b4c587dc0ac8a91efe
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-05-28 18:22:47 +0000

    Create String Compression III.cpp

---
+++ String Compression III.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        int count =0;
+        int i=0, j=0, n = word.size();
+        string ans ="";
+        while(j<word.length()){
+           count = 0;
+           while(j<n && word[i]==word[j] && count<9) {
+             count++;
+             j++;
+           }
+           ans += to_string(count) + word[i];
+           i = j;
+        }
+        return ans;
+    }
+};
