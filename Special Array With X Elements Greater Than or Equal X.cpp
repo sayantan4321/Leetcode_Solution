2c737443129e252ade2ed5b2eda0d4ec83249988
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-06-02 18:57:47 +0530

    Update Special Array With X Elements Greater Than or Equal X.cpp
    You are given an array nums of non-negative integers. nums is considered special if there exists a number x such that there are exactly x numbers in nums that are greater than or equal to x.
    
    Notice that x does not have to be an element in nums.
    
    Return x if the array is special, otherwise, return -1. It can be proven that if nums is special, the value for x is unique.
    
     
    
    Example 1:
    
    Input: nums = [3,5]
    Output: 2
    Explanation: There are 2 values (3 and 5) that are greater than or equal to 2.
    Example 2:
    
    Input: nums = [0,0]
    Output: -1
    Explanation: No numbers fit the criteria for x.
    If x = 0, there should be 0 numbers >= x, but there are 2.
    If x = 1, there should be 1 number >= x, but there are 0.
    If x = 2, there should be 2 numbers >= x, but there are 0.
    x cannot be greater since there are only 2 numbers in nums.
    Example 3:
    
    Input: nums = [0,4,3,0,4]
    Output: 3
    Explanation: There are 3 values that are greater than or equal to 3.
     
    
    Constraints:
    
    1 <= nums.length <= 100
    0 <= nums[i] <= 1000

--- Special Array With X Elements Greater Than or Equal X.cpp
+++ Special Array With X Elements Greater Than or Equal X.cpp
@@ -20,7 +20,7 @@ public:
     }
 };
 // OPTIMAL 
-// Approach - 1 ( Sort + Binary Search)
+// Approach - 1 ( Sort + Binary Search )
 class Solution {
 public:
     int lowerBound(vector<int>& nums, int x){
@@ -48,4 +48,29 @@ public:
     }
 };
 
+// Aprroach - 2 ( Cummulative Sum + Freq array )
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int n = nums.size();
+        /*  Every number larger than N is useless for our purpose,
+            and we can treat those numbers as N. 
+        */
+        vector<int> freq(n+1);
 
+        for(int i = 0; i < n; i++) {
+            freq[min(n, nums[i])]++;
+        }
+        int c_sum = 0;
+        for(int i = n; i >= 0; i--) {
+            c_sum += freq[i];
+            if(i == c_sum) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
