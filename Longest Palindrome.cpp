919055cd99bff7e647d212323b12822bb89b1d00
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-06-06 00:59:11 +0530

    Update Longest Palindrome.cpp
    Given a string s which consists of lowercase or uppercase letters, return the length of the longest 
    palindrome
     that can be built with those letters.
    
    Letters are case sensitive, for example, "Aa" is not considered a palindrome.
    
     
    
    Example 1:
    
    Input: s = "abccccdd"
    Output: 7
    Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
    Example 2:
    
    Input: s = "a"
    Output: 1
    Explanation: The longest palindrome that can be built is "a", whose length is 1.
     
    
    Constraints:
    
    1 <= s.length <= 2000
    s consists of lowercase and/or uppercase English letters only.

--- Longest Palindrome.cpp
+++ Longest Palindrome.cpp
@@ -9,9 +9,9 @@ public:
             mpp[s[i]]++;
         }
         for(auto it : mpp){
-            cnt += (it.second/2) * 2;
+            cnt += (it.second/2) * 2; // to overcome the case of similar digit odd number count
             // if(it.second % 2 == 0){
-            //     cnt += (it.second/2) * 2;
+            //     cnt += (it.second/2) * 2; // "ccc" it falls under odd but the count of 2 c's are overlooked
             // }
             if(it.second % 2 == 1){
                 hasOne = true;
