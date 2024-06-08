4882989c4691946998be363ab866681b29591190
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-06-09 01:45:40 +0530

    Update Hand of Straights.cpp

--- Hand of Straights.cpp
+++ Hand of Straights.cpp
@@ -1,4 +1,6 @@
-
+// Approach -1
+// T.C. -> O(N)
+// S.C. -> O(N)    
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
@@ -19,6 +21,38 @@ public:
                 if(mpp[curr+i] < 1){
                     mpp.erase(curr+i);
                 }
+            }
+        }
+        return true;
+    }
+};
+
+// Approach -2
+class Solution {
+public:
+    bool findSuccessors(vector<int>& hand, int groupSize, int i, int n) {
+        int next = hand[i] + 1;
+        hand[i] = -1;  // Mark as used
+        int count = 1;
+        i += 1;
+        while (i < n && count < groupSize) {
+            if (hand[i] == next) {
+                next = hand[i] + 1;
+                hand[i] = -1;
+                count++;
+            }
+            i++;
+        }
+        return count == groupSize;
+    }
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        if(n % groupSize != 0) return false;
+
+        sort(hand.begin(), hand.end());
+        for (int i = 0; i < n; i++) {
+            if (hand[i] >= 0) {
+                if (!findSuccessors(hand, groupSize, i, n)) return false;
             }
         }
         return true;
