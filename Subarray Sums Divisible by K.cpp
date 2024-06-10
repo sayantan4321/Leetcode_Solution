09ac090b3b9f6400f077c18497caf955cf3ea54f
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-06-10 23:35:24 +0530

    Update Subarray Sums Divisible by K.cpp

--- Subarray Sums Divisible by K.cpp
+++ Subarray Sums Divisible by K.cpp
@@ -1,1 +1,29 @@
-ndwjnwjnfbw
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n= nums.size(), cnt=0;
+        int presum=0;
+        unordered_map<int, int> mpp;
+        mpp[0]=1;
+        for(int i=0; i<n; i++){
+            presum+= nums[i];
+            int rem= presum%k;
+            if(rem<0){
+                rem+=k;
+            }
+            if(mpp.find(rem)!=mpp.end()){
+                cnt+= mpp[rem];
+            }
+            mpp[rem]++;
+        }
+        return cnt;
+        // for(int i=0; i<n; i++){
+        //     int sum=0;
+        //     for(int j=i; j<n; j++){
+        //        sum+= nums[j];
+        //        if(sum%k==0) cnt++;
+        //     }
+        // }
+        // return cnt;
+    }
+};
