910bee85ac03013dcc1dff68cc8849b7032dffac
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-06-17 13:46:31 +0530

    Update Patching Array.cpp

--- Patching Array.cpp
+++ Patching Array.cpp
@@ -1,1 +1,19 @@
-gygtigi
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long maxReach = 0; // start from 1 we have to reach n with all in between numbers
+        int patch = 0;
+        int i = 0;
+
+        while(maxReach < n) {
+            if(i < nums.size() && nums[i] <= maxReach+1) {//when we are not able to reach maxreach+1
+               maxReach += nums[i]; // we will add the number
+               i++;
+            } else {
+               maxReach += (maxReach + 1); // if the maxreach+1 number is not in array 
+               patch++;
+            }
+        }
+        return patch;
+    }
+};
