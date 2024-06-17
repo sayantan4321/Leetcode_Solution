c041a15b15163f1dd072a1f5d08a2d3d9a8b068c
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-06-17 05:59:36 +0000

    Update IPO.cpp
    Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to Venture Capital, LeetCode would like to work on some projects to increase its capital before the IPO. Since it has limited resources, it can only finish at most k distinct projects before the IPO. Help LeetCode design the best way to maximize its total capital after finishing at most k distinct projects.
    
    You are given n projects where the ith project has a pure profit profits[i] and a minimum capital of capital[i] is needed to start it.
    
    Initially, you have w capital. When you finish a project, you will obtain its pure profit and the profit will be added to your total capital.
    
    Pick a list of at most k distinct projects from given projects to maximize your final capital, and return the final maximized capital.
    
    The answer is guaranteed to fit in a 32-bit signed integer.
    
     
    
    Example 1:
    
    Input: k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]
    Output: 4
    Explanation: Since your initial capital is 0, you can only start the project indexed 0.
    After finishing it you will obtain profit 1 and your capital becomes 1.
    With capital 1, you can either start the project indexed 1 or the project indexed 2.
    Since you can choose at most 2 projects, you need to finish the project indexed 2 to get the maximum capital.
    Therefore, output the final maximized capital, which is 0 + 1 + 3 = 4.
    Example 2:
    
    Input: k = 3, w = 0, profits = [1,2,3], capital = [0,1,2]
    Output: 6
     
    
    Constraints:
    
    1 <= k <= 10^5
    0 <= w <= 10^9
    n == profits.length
    n == capital.length
    1 <= n <= 10^5
    0 <= profits[i] <= 10^4
    0 <= capital[i] <= 10^9

--- IPO.cpp
+++ IPO.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        if(k > profits.size() && profits.size()==capital.size()) return 0;
-        vector<pair<int,int>> maxp;
+        int n = capital.size();
+        if(profits.size() != capital.size()) return 0;
+        vector<pair<int,int>> maxp(n); // storing corresponding capital-profit as pair
         priority_queue<int> pq;
-        for(int i=0; i<capital.size(); i++){
-            maxp.push_back(capital[i], profits[i]);
+        for(int i=0; i<n; i++){
+            maxp[i] = {capital[i], profits[i]};
         }
-        sort(capital.begin(), capital.end());
-        for(int i=0; i<capital.size(); i++){
-            if(capital[i] == w){
-                w += profits[i];
-                k--;
-                capital[i] = -1;
+        sort(maxp.begin(), maxp.end()); // sort based on capital{first}
+        int i=0;
+
+        while(k--){
+            while(i < n && maxp[i].first <= w){// the capital which was less than w will choosen and added 
+                pq.push(maxp[i].second);       // in PQ. PQ will ensure the max profit.
+                i++;
             }
+            if(pq.empty()){
+                break;   // if for first time also no capital[i] is matched with w
+            }
+            w += pq.top();//if capital=0  and profits are 1 andd 2, then 2 will be in front and it will pop out.
+            pq.pop();        // Also added in w as a capital
         }
+        return w;
     }
 };
