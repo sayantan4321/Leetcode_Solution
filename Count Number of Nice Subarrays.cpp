83d450ea5c54f86f94386550156750d97f85521b
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-06-23 15:30:09 +0530

    Update Count Number of Nice Subarrays.cpp
    Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
    
    Return the number of nice sub-arrays.
    
     
    
    Example 1:
    
    Input: nums = [1,1,2,1,1], k = 3
    Output: 2
    Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
    Example 2:
    
    Input: nums = [2,4,6], k = 1
    Output: 0
    Explanation: There are no odd numbers in the array.
    Example 3:
    
    Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
    Output: 16
     
    
    Constraints:
    
    1 <= nums.length <= 50000
    1 <= nums[i] <= 10^5
    1 <= k <= nums.length

--- Count Number of Nice Subarrays.cpp
+++ Count Number of Nice Subarrays.cpp
@@ -1,3 +1,6 @@
+// Approach - Prefix Sum + MAP
+// T.C. -> O(N)
+// S.C. -> O(1)
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
@@ -20,6 +23,9 @@ public:
     }
 };
 
+// Approach - Two Pointers [Sliding Window]
+// T.C. -> O(N)
+// S.C. -> O(N)
 class Solution {
 public:
     // int cntofOdd(vector<int>& nums, int start, int end){
@@ -55,21 +61,4 @@ public:
         return result;
     }
 };
-
-/* unordered_map<int, int>mpp;
-        int oddcnt = 0;
-        int result = 0;
-       // Initialize the map with 0 count to handle the subarrays starting from index 0
-        mpp[0] = 1;  // count of odd numbers = count their freq(how many times we have seen it)
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]%2 !=0){
-                oddcnt++;
-            }
-            // If there are `oddCount - k` odd numbers before, add their count to the result
-            if(mpp.find(oddcnt - k) != mpp.end()){
-                result += mpp[oddcnt -k];
-            }
-            mpp[oddcnt]++;
-        }
-        return result; */
 
