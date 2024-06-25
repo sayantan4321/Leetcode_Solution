11ee005a177400ee686e9f88e44977b782bee1e7
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-06-25 11:21:47 +0530

    Update Most Profit Assigning Work.cpp
    You have n jobs and m workers. You are given three arrays: difficulty, profit, and worker where:
    
    difficulty[i] and profit[i] are the difficulty and the profit of the ith job, and
    worker[j] is the ability of jth worker (i.e., the jth worker can only complete a job with difficulty at most worker[j]).
    Every worker can be assigned at most one job, but one job can be completed multiple times.
    
    For example, if three workers attempt the same job that pays $1, then the total profit will be $3. If a worker cannot complete any job, their profit is $0.
    Return the maximum profit we can achieve after assigning the workers to the jobs.
    
     
    
    Example 1:
    
    Input: difficulty = [2,4,6,8,10], profit = [10,20,30,40,50], worker = [4,5,6,7]
    Output: 100
    Explanation: Workers are assigned jobs of difficulty [4,4,6,6] and they get a profit of [20,20,30,30] separately.
    Example 2:
    
    Input: difficulty = [85,47,57], profit = [24,66,99], worker = [40,25,25]
    Output: 0
     
    
    Constraints:
    
    n == difficulty.length
    n == profit.length
    m == worker.length
    1 <= n, m <= 10^4
    1 <= difficulty[i], profit[i], worker[i] <= 10^5

--- Most Profit Assigning Work.cpp
+++ Most Profit Assigning Work.cpp
@@ -1,13 +1,42 @@
+//Approach-1 (Using max-heap)
+//T.C : O(nlogn + mlogn)
+//S.C : O(n)
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        int n = difficulty.size();
+        int m = worker.size();
 
+        priority_queue<pair<int, int>> pq; //max heap of pairs
 
+        for(int i = 0; i < n; i++) {
+            int diff = difficulty[i];
+            int prof = profit[i];
 
+            pq.push({prof, diff});
+        }
 
+        sort(begin(worker), end(worker), greater<int>()); //descending order
 
+        int i = 0;
+        int totalProfit = 0;
+        while(i < m && !pq.empty()) {
+            if(pq.top().second > worker[i]) {
+                pq.pop();
+            } else {
+                totalProfit += pq.top().first;
+                i++;
+            }
+        }
 
-
-
+        return totalProfit;
+        
+    }
+};
 
 // Approach -1 --> Binary Search
+//T.C : O(nlogn + mlogn)
+//S.C : O(1)
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
