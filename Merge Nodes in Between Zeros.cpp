8670df398e43b685f2e38389909dd9127e6e9ca6
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-07-06 00:17:41 +0530

    Create Merge Nodes in Between Zeros.cpp

---
+++ Merge Nodes in Between Zeros.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ios::sync_with_stdio(false); 
+        ListNode* temp = head->next; // Skip the first 0 node
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
+        int total = 0;
+        
+        while (temp != nullptr) {
+            if (temp->val == 0) {
+                // Create a new node with the sum and add it to the result list
+                ListNode* newNode = new ListNode(total);
+                curr->next = newNode;
+                curr = curr->next;
+                // Reset the total for the next segment
+                total = 0;
+            } 
+            else {
+                // Add the value to the total sum
+                total += temp->val;
+            }
+            temp = temp->next;
+        }
+        return dummy->next;
+    }
+};
