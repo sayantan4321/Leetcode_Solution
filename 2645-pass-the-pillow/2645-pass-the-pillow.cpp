f774494763b663fa53bb161e41e460b4a03b038d
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-07-06 20:46:53 +0000

    Time: 2 ms (54.3%), Space: 7.1 MB (31.2%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        // int pos = 0, i=0;
+        // while(time >= 0){
+        //     for(int )
+        // }
+        int div = time / (n-1);
+        int rem = time % (n-1);
+
+        if (rem == 0) {
+            if (div % 2 == 0) {
+                return 1; // Returns to the first person if even division
+            } else {
+                return n; // Returns to the last person if odd division
+            }
+        } 
+        else {
+            if (div % 2 == 0) {
+                return 1 + rem; // Move forward if even division
+            } else {
+                return n - rem; // Move backward if odd division
+            }
+        }
+
+    }
+};
