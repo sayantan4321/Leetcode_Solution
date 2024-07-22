57f027dd669358a8036282dc0ab6c6434ce5ad59
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-07-22 08:29:42 +0530

    Update Sort the People.cpp

--- Sort the People.cpp
+++ Sort the People.cpp
@@ -1,3 +1,8 @@
+// Approach - 1
+// space is same as given array size
+
+// T.C. -> O(N)
+// S.C. -> O(1)
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
@@ -22,5 +27,28 @@ public:
             res.push_back(names[curr]);
         }
         return res;
+    }
+};
+
+// Ussing Ordered Map (Approach -2)
+// S.C. O(N)
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        map<int,string> mp;
+        int n = heights.size();
+
+        for(int i=0;i<n;i++){
+            mp[heights[i]]  = names[i];
+        }
+        vector<string> ans;
+        auto it = mp.end();
+        it--;
+        while(it != mp.begin()){
+            ans.push_back(it->second);
+            it--;  
+        }
+        ans.push_back(it->second);
+        return ans;
     }
 };
